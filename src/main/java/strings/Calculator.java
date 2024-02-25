58e824a408294e21cf9d0a286890d0974066f6f2
jagansai <jagan.sai@outlook.com>
2024-02-25 19:43:58 +0800

    Creating CalculatorV1 and adding stub for CalculatorV2

--- src/main/java/strings/Calculator.java
+++ src/main/java/strings/Calculator.java
@@ -1,5 +1,7 @@
 package main.java.strings;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     /**
@@ -27,6 +29,73 @@ public class Calculator {
         // accept the numbers in a string
         // split them
         // and then add the numbers if the user chose 'Add'
+        while(true){
+            System.out.println("1.Add");
+            System.out.println("2.Subtract");
+            System.out.println("3.Multiple");
+            System.out.println("4.Division");
+            System.out.println("5.exit");
+
+            System.out.print("enter a choice: ");
+            Scanner sc = new Scanner(System.in);
+            int choice= sc.nextInt();
+            if (choice==5){
+                break;
+            }
+            if (choice==1){
+                add();
+            }
+            if (choice==2){
+                Subtract();
+            }
+            if (choice==3){
+                Multiple();
+            }
+            if (choice==4){
+                Division();
+            }
+
+
+        }
+    }
+    private static void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.print("enter num2: ");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+
+    }
+    private static void Subtract(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.print("enter num2: ");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+
+    }
+    private static void Multiple(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.print("enter num2: ");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+
+    }
+    private static void Division(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.print("enter num2: ");
+        int num2 = sc.nextInt();
+        if(num2==0){
+            System.out.println("cannot be divide by 0");
+        }else{
+            System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/strings/CalculatorV2.java
@@ -0,0 +1,26 @@
+package main.java.strings;
+
+/**
+ * In Version 1, we asked the user to enter the numbers 1 after the other.
+ * It worked fine, but it was not quite natural. User was asked to enter few choices and then there was a reply.
+ * Instead, how about asking the user to make the calculations in  an expression format?
+ *
+ * For e.g. User enters like 👇🏻
+ *
+ *  > 1 + 2 ( Enter )
+ *  > 3
+ *  > 2 - 1 ( Enter )
+ *  > 1
+ *
+ */
+public class CalculatorV2 {
+
+    public static void main(String[] args) {
+        calculatorTest();
+    }
+
+    private static void calculatorTest() {
+
+    }
+
+}
